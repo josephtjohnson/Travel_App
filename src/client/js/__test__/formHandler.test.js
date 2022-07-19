da9699947a0bb1277afc438a1276e9b7d460a668
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-07-19 10:15:19 -0400

    Update testing for handleSubmit() and updateUI()

--- src/client/js/__test__/formHandler.test.js
+++ src/client/js/__test__/formHandler.test.js
@@ -1,6 +1,21 @@
+import { handleSubmit } from ../src/client/js/formHandler;
 import { inputValidation } from ../src/client/js/formHandler;
+import { updateUI } from ../src/client/js/formHandler;
 
 Describe(“Testing the submit functionality”, () => {
-test(“Testing the inputValidation() function”, () => {
-		     expect(inputValidation('Chicago', '11/02/2022', '11/03/2022', 11/02/2022, 11/03/2022).toBeTruthy();
-})});
+	it(“Verifies information posted”, async () => {
+		const res = await request.post('http://localhost:8081/trips')
+			.send([city: 'Chicago'});
+		expect(res.status).toEqual(200)	
+	});
+});
+Describe(“Testing the submit functionality”, () => {
+	test(“Testing the updateUI() function”, () => {
+		expect(updateUI('Chicago', '11/02/2022', '11/03/2022', 1).toBeDefined();
+	});
+});
+Describe(“Testing the submit functionality”, () => {
+	test(“Testing the inputValidation() function”, () => {
+		expect(inputValidation('Chicago', '11/02/2022', '11/03/2022', 11/02/2022, 11/03/2022).toBeTruthy();
+	});
+});
