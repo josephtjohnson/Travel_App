37d1f329bd528a8ca01e81a6762526216bdec72a
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-07-19 09:30:46 -0400

    Update describe messages

--- src/client/js/__test__/formChecker.test.js
+++ src/client/js/__test__/formChecker.test.js
@@ -2,17 +2,17 @@ import { checkForCity } from ../src/client/js/formChecker;
 import { checkForDate } from ../src/client/js/formChecker;
 import { chronos } from ../src/client/js/formChecker;
 
-Describe(“Testing the submit functionality”, () => {
+Describe(“Testing the city check functionality”, () => {
 test(“Testing the checkForCity() function”, () => {
-		     expect(checkForCity(city).toBeDefined();
+	expect(checkForCity(city).toBeDefined();
 })});
 
-Describe(“Testing the submit functionality”, () => {
+Describe(“Testing the date check functionality”, () => {
 test(“Testing the checkForDate() function”, () => {
-		     expect(checkForDate(startDate, endDate)).toBeDefined();
+	expect(checkForDate(startDate, endDate)).toBeDefined();
 })});
 
-Describe(“Testing the submit functionality”, () => {
+Describe(“Testing the trip length functionality”, () => {
 test(“Testing the chronos() function”, () => {
-		     expect(chronos(startDate, endDate)).toBeTruthy;
+	expect(chronos(startDate, endDate)).toBeTruthy;
 })});
