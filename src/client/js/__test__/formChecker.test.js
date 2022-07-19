c832bf372b22f020a6be41c1766e742474840fdd
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-07-19 10:16:28 -0400

    Update formatting

--- src/client/js/__test__/formChecker.test.js
+++ src/client/js/__test__/formChecker.test.js
@@ -3,16 +3,19 @@ import { checkForDate } from ../src/client/js/formChecker;
 import { chronos } from ../src/client/js/formChecker;
 
 Describe(“Testing the city check functionality”, () => {
-test(“Testing the checkForCity() function”, () => {
-	expect(checkForCity(Chicago).toBeTruthy();
-})});
+	test(“Testing the checkForCity() function”, () => {
+		expect(checkForCity(Chicago).toBeTruthy();
+	});
+});
 
 Describe(“Testing the date check functionality”, () => {
-test(“Testing the checkForDate() function”, () => {
-	expect(checkForDate(11/02/2022, 11/03/2022)).toBeTruthy();
-})});
+	test(“Testing the checkForDate() function”, () => {
+		expect(checkForDate(11/02/2022, 11/03/2022)).toBeTruthy();
+	});
+});
 
 Describe(“Testing the trip length functionality”, () => {
-test(“Testing the chronos() function”, () => {
-	expect(chronos(11/02/2022, 11/03/2022)).toBeTruthy;
-})});
+	test(“Testing the chronos() function”, () => {
+		expect(chronos(11/02/2022, 11/03/2022)).toBeTruthy;
+	});
+});
