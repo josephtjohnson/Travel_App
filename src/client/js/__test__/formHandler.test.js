31a1bcd364fc115ff3a9c3b30d54262a9c0ccf81
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-07-19 09:50:18 -0400

    Create formHandler.test.js

---
+++ src/client/js/__test__/formHandler.test.js
@@ -0,0 +1,6 @@
+import { inputValidation } from ../src/client/js/formHandler;
+
+Describe(“Testing the submit functionality”, () => {
+test(“Testing the inputValidation() function”, () => {
+		     expect(inputValidation('Chicago', '11/02/2022', '11/03/2022', 11/02/2022, 11/03/2022).toBeTruthy();
+})});
