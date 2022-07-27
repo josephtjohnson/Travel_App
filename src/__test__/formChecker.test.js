e41989e532d62decc5e35d0a05511fa281e7bc0d
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-07-27 08:43:39 -0400

    Add describe statement

--- src/__test__/formChecker.test.js
+++ src/__test__/formChecker.test.js
@@ -2,8 +2,10 @@ import{ checkForCity } from '../src/client/js/formChecker';
 //const { checkForDate } = require('../src/client/js/formChecker');
 //const { chronos } = require('../src/client/js/formChecker');
 
-test('Testing the checkForCity() function', () => {
-  expect(checkForCity(Chicago)).toBeTruthy()
+describe('Testing checkForCity() function', () => {
+  test('Testing the checkForCity() function', () => {
+    expect(checkForCity(Chicago)).toBeTruthy()
+  });
 });
 
 //describe("Testing the date check functionality", () => {
@@ -16,4 +18,6 @@ test('Testing the checkForCity() function', () => {
 //	test("Testing the chronos() function", () => {
 //		expect(chronos(11/02/2022, 11/03/2022)).toBeTruthy()
 //	});
-//});
+
+
+ 
