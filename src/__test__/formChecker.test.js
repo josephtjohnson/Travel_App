7bb38380520be5e9ff27b00f8dee3b2e018cab11
Joseph Johnson <johnson_jt@outlook.com>
2022-08-26 18:19:09 -0400

    Resolve jest errors

--- src/__test__/formChecker.test.js
+++ src/__test__/formChecker.test.js
@@ -3,6 +3,6 @@ const checkForCity = require('../client/js/formChecker');
 describe('Testing checkForCity() function', () => {
 const city = 'Chicago';
   test('Testing the checkForCity() function', () => {
-    expect(checkForCity(city)).toBeTruthy()
+    expect(checkForCity).toBeTruthy()
   });
 });
